b2d44829d5b0ec85d56055ed0ebadfaa97142e4c
Nicolas Pognante <nicolas.pognante@hotmail.fr>
2021-05-24 15:45:56 +0200

    rails new

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @ask = params[:ask]
+    @answer = if @ask == 'I am going to work'
+                'Great !'
+              elsif @ask.include?('?')
+                'Silly question, get dressed and go to work!.'
+              else
+                "I don't care, get dressed and go to work!"
+              end
+  end
+end
